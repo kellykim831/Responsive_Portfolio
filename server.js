bc14aa39c063efc806e263e341e7b45f546bbc3b
Kelly Kim <kellykim408@gmail.com>
2020-11-04 21:59:52 -0800

    use express routes

---
+++ server.js
@@ -0,0 +1,42 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/html/index.html"));
+});
+
+app.get("/portfolio", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/html/portfolio.html"));
+});
+
+
+app.get("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/html/contact.html"));
+});
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
